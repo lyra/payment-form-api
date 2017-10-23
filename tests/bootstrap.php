c549b97442d19be8ca922b9ea1d770ffc34f610a
Nabil Semaoune <nabil.semaoune@lyra-network.com>
2017-10-23 12:06:28 +0200

    Improve license notices.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,13 @@
 <?php
 /**
+ * Copyright (C) 2017 Lyra Network.
  * This file is part of Lyra payment form API.
- * Copyright (C) Lyra Network.
+ *
  * See COPYING.txt for license details.
+ *
+ * @author    Lyra Network <contact@lyra-network.com>
+ * @copyright 2017 Lyra Network
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL v3)
  */
 
 require_once __DIR__ . '/../init.php';
